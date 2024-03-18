fda58f262e4e9814a369298a105a97b948851507
edsonalcala <edson_alcala@msn.com>
2024-03-18 10:21:12 -0600

    removed local and test configs for now

--- src/config/Configuration.sol
+++ src/config/Configuration.sol
@@ -4,10 +4,8 @@ pragma solidity 0.8.19;
 import {Constants} from "@constants/Constants.sol";
 import {DeploymentUtils} from "@utils/DeploymentUtils.sol";
 
-import {ConfigurationLocal} from "./Configuration.local.sol";
 import {ConfigurationMainnet} from "./Configuration.mainnet.sol";
 import {ConfigurationTestnet} from "./Configuration.testnet.sol";
-import {ConfigurationTest} from "./Configuration.test.sol";
 
 import {Vm} from "forge-std/Vm.sol";
 
@@ -28,14 +26,6 @@ library Configuration {
 
         if (_networkId == Constants.ETHEREUM_SEPOLIA_NETWORK) {
             return ConfigurationTestnet.getConfig();
-        }
-
-        if (_networkId == Constants.LOCAL_NETWORK) {
-            return ConfigurationLocal.getConfig();
-        }
-
-        if (_networkId == Constants.LOCAL_TEST_NETWORK) {
-            return ConfigurationTest.getConfig();
         }
 
         revert("Configuration: network not supported");
